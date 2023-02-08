4fb646980a33516079b9f259cc912d0d95a9cbf6
a1 <nadyagulikyan@gmail.com>
2023-02-08 23:46:14 +0500

    classes1

---
+++ src/Book.java
@@ -0,0 +1,51 @@
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private int publishingYear;
+    private Author author;
+
+    public Book(String title, int publishingYear, Author author) {
+        this.title = title;
+        this.publishingYear = publishingYear;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        this.publishingYear = publishingYear;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Название книги: " + title + ",год издания: " + publishingYear + ", автор: " + author;
+
+    }
+    @Override
+    public int hashCode(){
+       return Objects.hash(title, publishingYear,author);
+
+    }
+}
+
+
+
