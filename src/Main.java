786749ed653801092d34fff8155a4cf14311f8cf
a1 <nadyagulikyan@gmail.com>
2023-02-08 22:59:10 +0500

    classes

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        Author Pushkin = new Author("Александр", "Пушкин");
+        Author Tolstoy = new Author("Лев", "Толстой");
+        Author Tolstoy2 = new Author("Лев", "Толстой");
+
+        System.out.println(Tolstoy.equals(Tolstoy2));
+
+
+
+        Book prorok = new Book("Пророк", 1828, Pushkin);
+        Book warANDPeace = new Book("Война и мир", 1873, Tolstoy);
+
+        System.out.println(prorok);
+        System.out.println(warANDPeace);
+
+        prorok.setPublishingYear(1828);
+        System.out.println(prorok);
+
+        System.out.println(prorok.hashCode());
+        System.out.println(warANDPeace.hashCode());
+
+
+
+
+    }
+}
